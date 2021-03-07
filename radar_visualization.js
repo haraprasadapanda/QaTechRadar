a18a0a961268e2878fac424e23f65437a1859ed8
Hara Prasada Panda <harpa@yousee.dk>
2021-03-07 23:37:06 +0100

    first commit

---
+++ radar_visualization.js
@@ -0,0 +1,694 @@
+radar_visualization({
+  svg_id: "radar",
+  width: 1850,
+  height: 1000,
+  colors: {
+    
+    background: "radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)",
+    grid: "#808080",
+    inactive: "#ddd"
+  },
+  title: "Nuuday QA Radar",
+  quadrants: [
+    { name: "Languages" },
+    { name: "Infrastructure" },
+    { name: "Frameworks" },
+    { name: "Data Management" }
+  ],
+  rings: [
+    { name: "ADOPT", color: "#00FF7F" },
+    { name: "TRIAL", color: "#FFFF00" },
+    { name: "ASSESS", color: "#FFD700" },
+    { name: "HOLD", color: "#FFA500" }
+  ],
+  print_layout: true,
+  // zoomed_quadrant: 0,
+  //ENTRIES
+  entries: [
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "X",
+      active: false,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 1,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 1,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 1,
+      label: "X",
+      active: false,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 1,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 1,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 2,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 2,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 3,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "X",
+      active: false,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "X",
+      active: false,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "X",
+      active: false,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "X",
+      active: false,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 1,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 1,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 2,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 2,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 2,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 3,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 3,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 3,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 3,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 3,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 3,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 3,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 3,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 0,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 0,
+      label: "X",
+      active: false,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 0,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 0,
+      label: "X",
+      active: false,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 0,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 0,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 0,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 1,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 1,
+      label: "X",
+      active: false,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 1,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 1,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 1,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 1,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 1,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 1,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 2,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 2,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 2,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 2,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 2,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 2,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 2,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 2,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 3,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 3,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 3,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 3,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 3,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 0,
+      label: "X",
+      active: false,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 0,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 0,
+      label: "X",
+      active: false,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 0,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 0,
+      label: "X",
+      active: false,
+      moved: 1
+    },
+    {
+      quadrant: 1,
+      ring: 0,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 0,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 1,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 1,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 2,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 3,
+      label: "X",
+      active: false,
+      link: "",
+      moved: -1
+    },
+    {
+      quadrant: 0,
+      ring: 0,
+      label: "X",
+      active: true,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 0,
+      ring: 0,
+      label: "X",
+      active: true,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 0,
+      ring: 0,
+      label: "X",
+      active: true,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 0,
+      ring: 0,
+      label: "X",
+      active: true,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 0,
+      ring: 0,
+      label: "X",
+      active: true,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 0,
+      ring: 0,
+      label: "X",
+      active: true,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 0,
+      ring: 0,
+      label: "X",
+      active: true,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 0,
+      ring: 3,
+      label: "X",
+      active: true,
+      link: "",
+      moved: -1
+    },
+    {
+      quadrant: 0,
+      ring: 1,
+      label: "X",
+      active: true,
+      link: "",
+      moved: 1
+    },
+    {
+      quadrant: 0,
+      ring: 3,
+      label: "X",
+      active: true,
+      link: "",
+      moved: -1
+    },
+    {
+      quadrant: 0,
+      ring: 1,
+      label: "X",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 0,
+      ring: 1,
+      label: "X",
+      active: true,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 0,
+      ring: 2,
+      label: "X",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 0,
+      ring: 3,
+      label: "X",
+      active: true,
+      link: "",
+      moved: -1
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "X",
+      active: false,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "X",
+      active: false,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 1,
+      label: "X",
+      active: false,
+      link: "",
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 2,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 3,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 3,
+      label: "X",
+      active: false,
+      moved: 0
+    },
+  ]
+  //ENTRIES
+});
